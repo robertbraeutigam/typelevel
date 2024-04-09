40c710988b439206175b0911117d30e60b2ffeb2
Robert Brautigam <robert.brautigam@prewave.ai>
2024-04-09 10:47:56 +0200

    Fix plus

--- src/de/campus/typelevel/Peano.scala
+++ src/de/campus/typelevel/Peano.scala
@@ -5,11 +5,16 @@ object Peano {
   class _0             extends Nat
   class Succ[N <: Nat] extends Nat
 
-  type _1 = Succ[_0]
-  type _2 = Succ[_1]
-  type _3 = Succ[_2]
-  type _4 = Succ[_3]
-  type _5 = Succ[_4]
+  type _1  = Succ[_0]
+  type _2  = Succ[_1]
+  type _3  = Succ[_2]
+  type _4  = Succ[_3]
+  type _5  = Succ[_4]
+  type _6  = Succ[_5]
+  type _7  = Succ[_6]
+  type _8  = Succ[_7]
+  type _9  = Succ[_8]
+  type _10 = Succ[_9]
 
   // Plus
   trait +[A <: Nat, B <: Nat] {
@@ -19,9 +24,10 @@ object Peano {
   object + {
     type PlusFull[A <: Nat, B <: Nat, R <: Nat] = +[A, B] { type Result = R }
 
-    given basic[N <: Nat]: PlusFull[_0, N, N]                                                                        = new +[_0, N] {
-      override type Result = N
-    }
+    given basic[N <: Nat]: PlusFull[_0, N, N]                                                                        =
+      new +[_0, N] {
+        override type Result = N
+      }
     given inductive[A <: Nat, N <: Nat, R <: Nat](using induction: PlusFull[A, N, R]): PlusFull[Succ[A], N, Succ[R]] =
       new +[Succ[A], N] {
         override type Result = Succ[R]
