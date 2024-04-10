de4850d56e4b48bb6defdab113b3edfc71f620c4
Robert Brautigam <robert.brautigam@prewave.ai>
2024-04-10 16:55:55 +0200

    Progress

--- src/de/campus/typelevel/Natural.scala
+++ src/de/campus/typelevel/Natural.scala
@@ -1,6 +1,10 @@
 package de.campus.typelevel
 
 import de.campus.typelevel.Peano.*
-import de.campus.typelevel.PeanoOps.*
 
-// What's a "Natural"?
+class Natural[MIN <: Nat, MAX <: Nat](val value: BigInt)(using MIN <= MAX)
+
+trait <=[MIN <: Nat, MAX <: Nat]
+
+given basic[MAX <: Nat]: <=[_0, MAX] with                                                      {}
+given inductive[MIN <: Nat, MAX <: Nat](using ev: <=[MIN, MAX]): <=[Succ[MIN], Succ[MAX]] with {}
--- src/de/campus/typelevel/Scratchpad.scala
+++ src/de/campus/typelevel/Scratchpad.scala
@@ -1,6 +1,13 @@
 package de.campus.typelevel
 
-import de.campus.typelevel.Natural.*
 import de.campus.typelevel.Peano.*
 
-object Scratchpad extends App {}
+object Scratchpad extends App {
+  val zero = new Natural[_0, _0](0)
+  val one  = new Natural[_1, _1](1)
+  val two  = new Natural[_2, _2](2)
+
+  def readSensor(): Natural[_0, _10] = ???
+
+  val strange = new Natural[_10, _0](0)
+}
