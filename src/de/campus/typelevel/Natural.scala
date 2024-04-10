a49508a66667bcdf6a15390280f23f4c5a028fd5
Robert Brautigam <robert.brautigam@prewave.ai>
2024-04-10 17:26:17 +0200

    Remove require

--- src/de/campus/typelevel/Natural.scala
+++ src/de/campus/typelevel/Natural.scala
@@ -3,17 +3,4 @@ package de.campus.typelevel
 import de.campus.typelevel.Peano.*
 import de.campus.typelevel.PeanoOps.*
 
-class Natural[MIN <: Nat, MAX <: Nat](val value: BigInt)(using MIN <= MAX)(using minValue: BigIntValueOf[MIN]) {
-  require(minValue.value <= value, s"Value $value less than MIN ${minValue.value}")
-}
-
-trait BigIntValueOf[N <: Nat] {
-  val value: BigInt
-}
-
-given zeroCase: BigIntValueOf[_0] with                                                   {
-  override val value: BigInt = 0
-}
-given inductiveCase[N <: Nat](using prev: BigIntValueOf[N]): BigIntValueOf[Succ[N]] with {
-  override val value: BigInt = prev.value + 1
-}
+class Natural[MIN <: Nat, MAX <: Nat](val value: BigInt)(using MIN <= MAX)
