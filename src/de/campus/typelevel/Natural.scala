12244afbcc67c6ba4627c5fa28e7463fb045f2a2
Robert Brautigam <robert.brautigam@prewave.ai>
2024-04-10 11:51:12 +0200

    Remove macro

--- src/de/campus/typelevel/Natural.scala
+++ src/de/campus/typelevel/Natural.scala
@@ -1,7 +1,6 @@
 package de.campus.typelevel
 
 import de.campus.typelevel.Peano.*
-import scala.quoted.*
 
 case class Natural[MIN <: Nat, MAX <: Nat](value: BigInt) {
   def +[MIN2 <: Nat, MAX2 <: Nat](
@@ -13,31 +12,5 @@ case class Natural[MIN <: Nat, MAX <: Nat](value: BigInt) {
       println("Adding high numbers...")
     }
     Natural(value + other.value)
-  }
-}
-
-object Natural {
-  transparent inline def toNatural(inline n: Int): Any = ${ toNaturalMacro('n) }
-
-  def toNaturalMacro(n: Expr[Int])(using Quotes): Expr[Any] = {
-    import quotes.reflect.*
-
-    def peanoType(n: Int): TypeRepr = n match {
-      case 0 => TypeRepr.of[_0]
-      case _ => AppliedType(TypeRepr.of[Succ], List(peanoType(n - 1)))
-    }
-
-    def createNatural(n: Int): Expr[Any] = {
-      // report.error(s"Type for $n is ${peanoType(n)}")
-      peanoType(n).asType match {
-        case '[t] => '{ Natural[_0, _0](${ Expr(BigInt(n)) }) }
-      }
-    }
-
-    n.value match {
-      case Some(number) if number >= 0 => createNatural(number)
-      case Some(_)                     => report.errorAndAbort("Negative number cannot be converted to Natural number")
-      case None                        => report.errorAndAbort("Not a constant expression")
-    }
   }
 }
--- src/de/campus/typelevel/Scratchpad.scala
+++ src/de/campus/typelevel/Scratchpad.scala
@@ -5,12 +5,10 @@ import de.campus.typelevel.Peano.*
 import de.campus.typelevel.Peano.>?.given
 
 object Scratchpad extends App {
-  val one = toNatural(0)
+  val one = Natural[_1, _1](1)
   val two = Natural[_2, _2](2)
 
   def readSensor(): Natural[_0, _10] = Natural(5)
 
   val r1 = one + two + one + two
-
-  println(s"$r1 (${r1.getClass})")
 }
