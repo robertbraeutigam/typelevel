9a8e304cf003c66964f56483df77178e01cf5593
Robert Brautigam <robert.brautigam@prewave.ai>
2024-04-10 21:54:49 +0200

    Implement plus

--- src/de/campus/typelevel/Natural.scala
+++ src/de/campus/typelevel/Natural.scala
@@ -3,4 +3,13 @@ package de.campus.typelevel
 import de.campus.typelevel.Peano.*
 import de.campus.typelevel.PeanoOps.*
 
-class Natural[MIN <: Nat, MAX <: Nat](val value: BigInt)(using MIN <= MAX)
+class Natural[MIN <: Nat, MAX <: Nat](val value: BigInt)(using MIN <= MAX) {
+  def +[MIN2 <: Nat, MAX2 <: Nat](
+      other: Natural[MIN2, MAX2]
+  )(using
+      minSum: MIN + MIN2,
+      maxSum: MAX + MAX2,
+      ev: minSum.Result <= maxSum.Result
+  ): Natural[minSum.Result, maxSum.Result] =
+    Natural(value + other.value)
+}
--- src/de/campus/typelevel/Scratchpad.scala
+++ src/de/campus/typelevel/Scratchpad.scala
@@ -3,9 +3,11 @@ package de.campus.typelevel
 import de.campus.typelevel.Peano.*
 
 object Scratchpad extends App {
-  val zero = new Natural[_0, _0](0)
-  val one  = new Natural[_1, _1](1)
-  val two  = new Natural[_2, _2](2)
+  val zero = Natural[_0, _0](0)
+  val one  = Natural[_1, _1](1)
+  val two  = Natural[_2, _2](2)
 
   def readSensor(): Natural[_0, _10] = ???
+
+  val result = one + one + two
 }
